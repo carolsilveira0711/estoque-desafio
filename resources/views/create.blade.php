ba486513d1b947abcb6956287b5c3e13b944b5b6
carol <carolinasilveira0711@gmail.com>
2022-02-13 00:26:29 -0300

    Feature: create.blade.php - criação de produto

---
+++ resources/views/create.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cadastrar produto</title>
+</head>
+<body>
+<form action="">
+    <label for="">Nome</label><br>
+    <input type="text" name="nome"><br>
+    <label for="">Preço</label><br>
+    <input type="text" name="preco"><br>
+    <label for="">Quantidade</label><br>
+    <button>Salvar</button>
+</form>
+
+</body>
+</html>
